81ffb9bf968d6739b11df6b0ae930047e4377762
Jashanpreet <jashanpreet020105@gmail.com>
2024-03-25 19:06:12 -0500

    basic files

---
+++ assets/script/index.js
@@ -0,0 +1,80 @@
+import movies from './movies.js';
+
+
+const input = document.getElementById('input');
+const resultList = document.querySelector('.result-list');
+const button = document.getElementById('button');
+const movieContent = document.querySelector('.content');
+const movieTitle = document.querySelector('#movie-title');
+const movieDescription = document.querySelector('#movie-description');
+const poster = document.querySelector('#image');
+
+
+
+function validateInput(searchTerm) {
+    return searchTerm.length >= 2;
+}
+
+function searchMovies(searchTerm) {
+    const matches = movies.filter(movie => {
+        const title = movie.title.toLowerCase();
+        return title.includes(searchTerm);
+    });
+    return matches;
+}
+
+
+
+function displayMovies(matches) {
+    resultList.innerHTML = '';
+    if (matches.length > 0) {
+        const limitedMatches = matches.slice(0, 5);
+        const ul = document.createElement('ul');
+
+        limitedMatches.forEach(movie => {
+            const li = document.createElement('li');
+            li.textContent = movie.title;
+            li.addEventListener('click', function() {
+                displayMovieDetails(movie);
+                input.value = movie.title;
+                setTimeout(() => ul.innerHTML = '', 10);
+            });
+            ul.appendChild(li);
+        });
+
+        resultList.appendChild(ul);
+    } else {
+        resultList.innerHTML = '<li>Movie not found</li>';
+    }
+}
+
+
+function listMovies(searchTerm) {
+    const matches = searchMovies(searchTerm.toLowerCase());
+    displayMovies(matches);
+}
+
+
+function handleInput() {
+    const searchTerm = input.value.toLowerCase();
+    if (validateInput(searchTerm)) {
+        listMovies(searchTerm);
+    } else {
+        resultList.innerHTML = '';
+    }
+}
+
+input.addEventListener('input', handleInput);
+
+
+// Function to display details of a selected movie
+function displayMovieDetails(movie) {
+   movieTitle.innerHTML = movie.title;
+    movieDescription.innerHTML = `
+        <strong>Year:</strong> ${movie.year}<br>
+        <strong>Running Time:</strong> ${movie.runningTime}<br>
+        <strong>Description:</strong> ${movie.description}<br>
+        <strong>Genre:</strong> ${movie.genre}<br>
+    `;
+    poster.src = movie.poster;
+}
